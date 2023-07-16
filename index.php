0a15b2708e82457ce69236f7538b05c43b8e2c98
Abdullah KODAL <108393640+kodal035@users.noreply.github.com>
2023-07-16 22:04:39 +0300

    index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>KODAL035 E-Commerce</title>
+        <title>KODAL035</title>
 
         <!-- Favicon -->
         <link rel="icon" href="img/favicon.png" type="image/png" />
